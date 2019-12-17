b16075f335ee56874a65a693db6b64326e4f5458
saitotm <tomokazu.ml.tech@gmail.com>
2019-12-17 18:17:25 +0900

    画像表示機能追加

--- app/app.py
+++ app/app.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
 
-THRESHOLD = 1500
+level = [700, 1000, 2500]
 
 @app.route('/')
 def index():
-    if get_co2() > THRESHOLD:
-        message = '混雑しています'
-    else:
-        message = '空いています'
+    co2 = get_co2()
 
-    return render_template('index.html', state=message)
+    if co2 < level[0]:
+        img = 'img/face_0.png'
+        state = '特に問題はありません'
+
+    elif co2 < level[1]:
+        img = 'img/face_1.png'
+        state = '注意が必要です'
+    
+    elif co2 < level[2]:
+        img = 'img/face_2.png'
+        state = '眠気・不活発になるレベルです'
+    
+    else:
+        img = 'img/face_3.png'
+        state = '健康被害の出る可能性があります'
+        
+    return render_template('index.html', state=state, img=img, co2=co2)
 
 def get_co2():
-    return 1000
-
+    return 500
 
 app.run(port=8000, debug=True)
